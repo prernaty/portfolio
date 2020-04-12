a89695003dd09bfbd72021a2e8a3314ff8ea46af
prernaty <prerna.tyagi96@gmail.com>
2020-04-12 20:48:54 +0530

    Updates

---
+++ precache-manifest.186abba68afbc50ae07aaaf7bd82adb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98204bb0f8eb63a45b55f5a12be3503",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3ebbb7f24edfc94bef17",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "614404bd7fa5d033fdae",
+    "url": "/portfolio/static/css/main.b876e4ed.chunk.css"
+  },
+  {
+    "revision": "3ebbb7f24edfc94bef17",
+    "url": "/portfolio/static/js/2.46c86c6a.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.46c86c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614404bd7fa5d033fdae",
+    "url": "/portfolio/static/js/main.7a380410.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
